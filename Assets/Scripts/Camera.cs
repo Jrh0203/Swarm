e34b3b6ea7ecf19c44c775267150806f7ac2efc0
danielwong1 <daniel.wong@utexas.edu>
2018-11-09 16:43:05 -0600

    added new materials, player controls, and smooth camera follow

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    /*Note: Position should be based on player control
+    Rotation should be set at X: 45, Y: 135 Z:0*/
+    
+    //relative to the player
+    private Vector3 cameraDistVect;
+	private float smoothSpeed = .075f;
+    public const int DEFAULT_CAMERA_DIST = 30;
+	// Use this for initialization
+	void Start () {
+        //move the camera away from the player in the same direction it faces.
+        cameraDistVect = GameObject.FindWithTag("Player").transform.up * DEFAULT_CAMERA_DIST;
+	}
+	
+	void LateUpdate () {
+        //this.transform.position = cameraDistVect + GameObject.FindWithTag("Player").transform.position;
+
+		Vector3 desiredPos = cameraDistVect + GameObject.FindWithTag("Player").transform.position;
+		Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+		transform.position = smoothedPos;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public float playerSpeed;
+	private Vector3 playerVelocity;
+	private CharacterController controller;
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+		playerVelocity = new Vector3(0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 moveDir = GetMoveDirection();
+		Vector3 moveVelocity = moveDir * Time.deltaTime * playerSpeed;
+		
+		//set velocity in x and z plane
+		playerVelocity.x = moveVelocity.x;
+		playerVelocity.z = moveVelocity.z;
+
+		//set velocity in the y direction
+		playerVelocity += Physics.gravity * Time.deltaTime;
+		if(controller.isGrounded) {
+			playerVelocity.y = 0;
+		}
+
+		controller.Move(playerVelocity);
+	}
+
+	Vector3 GetMoveDirection() {
+		Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+		moveInput = Vector3.ClampMagnitude(moveInput, 1);
+		return moveInput;
+	}
+}
