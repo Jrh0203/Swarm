1cec4f5eb06d9dba4f3cab80a076687b5c0f5b2d
Ibrahim Abouzied <abouzied@utexas.edu>
2018-11-13 14:15:01 -0600

    Added navmesh and pathfinding script

---
+++ Assets/Scripts/EnemyPathFinding.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathFinding : MonoBehaviour {
+
+	public Transform goal;
+    private UnityEngine.AI.NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		InvokeRepeating("UpdateLocation", 0.0f, 0.3f); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void UpdateLocation() {
+		agent.destination = goal.position;
+	}
+}
