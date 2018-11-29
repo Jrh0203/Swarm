9799897b0f0d04c6e32d26b14057e1ed3ba38c66
crystalfire7 <12specialreindeers@gmail.com>
2018-11-29 03:52:50 -0600

    added main menu and level select gui

---
+++ Assets/Scripts/GUI/LevelButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelButton : MonoBehaviour {
+	public int levelNumber = 0;
+	public Button level;
+	// Use this for initialization
+	void Start () {
+		level = GetComponent<Button>();
+		level.onClick.AddListener(OpenLevel);
+	}
+	
+	void OpenLevel() {
+		string levelString = "Level" + levelNumber;
+		SceneManager.LoadScene(levelString);
+	}
+}
